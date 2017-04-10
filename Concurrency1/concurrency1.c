80def972c121fd9745675ed5d3c0ad490779a956
AlexDHoffer <hoffera@oregonstate.edu>
2017-04-09 20:57:44 -0700

    Structs to hold the data created. Have begun to inspect how to do the random number generation using ASM (ASM source file now included)>

---
+++ Concurrency1/concurrency1.c
@@ -0,0 +1,26 @@
+/* Alex Hoffer
+ * Concurrency 1 - Producer Consumer Problem
+ * April 2017
+ */
+
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "mt19937ar.c"
+
+typedef struct item
+{
+	int value1; // Just a number
+	int value2; // Random waiting period between 2 and 9 
+} item;
+
+
+
+int main()
+{
+	item i1;		
+
+
+	return 0;
+}
