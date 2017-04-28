451f24bc8ac315a8cf5aabeba197fb435247425d
AlexDHoffer <hoffera@oregonstate.edu>
2017-04-27 22:02:31 -0700

    Add nice I/O to prove correctness

--- Project2/whatever.c
+++ Project2/whatever.c
@@ -67,7 +67,7 @@ void eat(char* name, int leftForkPos, int rightForkPos)
 {
 	int random = (rand() % 8) + 2;
 	int randomTime = random * 1000000;
-	printf("Philosopher: %s. Seat position: %d. Status: EATING for %d seconds. Forks: HAS Left fork, position: %d. HAS Right fork, position: %d.\n", name, leftForkPos, random, leftForkPos, rightForkPos);
+	printf("Name: %s. Seat: %d. Status: EATING for %d seconds. Forks: HAS Left fork, position: %d. HAS Right fork, position: %d.\n", name, leftForkPos, random, leftForkPos, rightForkPos);
 	usleep(randomTime);
 }
 
@@ -76,8 +76,92 @@ void think(char* name, int pos)
 {
 	int random = (rand() % 20) + 1;
 	int randomTime = random * 1000000; // 1000000 microseconds is 1 second, usleep takes microseconds as input
-	printf("Philosopher: %s. Seat position: %d. Status: THINKING for %d seconds. Forks: HAS no forks.\n", name, pos, random);
+	printf("Name: %s. Seat: %d. Status: THINKING, %d seconds. Forks: HAS no forks.\n", name, pos, random);
 	usleep(randomTime);
+}
+
+// After eating, put the forks down. Lock the left and right forks, adjust the semaphore, unlock the forks
+void put_forks(char* philName, int philPosition, int rightForkPos)
+{
+	// Lock the forks
+	pthread_mutex_lock(&forkLocks[philPosition]);
+	pthread_mutex_lock(&forkLocks[rightForkPos]);
+	
+	// Adjust the semaphore: now the forks are available
+	forks[philPosition] = true;
+	forks[rightForkPos] = true;
+
+	// Unlock the forks now that the global resource is adjusted
+	pthread_mutex_unlock(&forkLocks[philPosition]);
+	pthread_mutex_unlock(&forkLocks[rightForkPos]);	
+
+	printf("Name: %s. Seat: %d. Status: FINISHED EATING, putting left fork (position %d), right fork (position %d) down.\n", philName, philPosition, philPosition, rightForkPos); 
+}
+	
+void get_forks(struct philosopher* phil, char* philName, int philPosition)
+{
+	printf("Name: %s. Seat: %d. Status: HUNGRY. Forks: Checking left fork, position %d.\n", philName, philPosition, philPosition);
+		
+	// Tentatively lock the philosopher's left fork in preparation for a feast.
+	// This'll fail if it's already locked.
+	pthread_mutex_lock(&forkLocks[philPosition]); 
+		
+	// Check the left fork using the shared bool resource that is locked from the statement above.
+	if (isForkAvailable(philPosition) == true) 
+	{
+		printf("Name: %s. Seat: %d. Status: HUNGRY. Forks: HAS left fork, position %d.\n", philName, philPosition, philPosition);		
+		// That fork is now unavailable. Alert the semaphore.
+		forks[philPosition] = false;
+
+		// Now that the semaphore is alerted, we have no need for the lock.
+		pthread_mutex_unlock(&forkLocks[philPosition]);
+
+		// Handle the right fork.
+		// Attempt to lock the global resources in order to modify them
+		// Check the semaphore to see if his right fork is free.
+		// If it is, unlock the global resources AFTER notifying the semaphore the right fork is no longer free.
+		// Then, lock both forks, change the global resources, then unlock the forks again.
+		int rightForkPos = getRightForkPosition(philPosition);
+
+		pthread_mutex_lock(&forkLocks[rightForkPos]);
+
+		printf("Name: %s. Seat: %d. Status: HUNGRY. Forks: HAS left fork, position %d, checking on right fork, position: %d.\n", philName, philPosition, philPosition, rightForkPos);
+			
+		// his right fork is free
+		if (isForkAvailable(rightForkPos) == true) 
+		{
+			forks[rightForkPos] = false;
+				
+			pthread_mutex_unlock(&forkLocks[rightForkPos]);
+
+			eat(philName, philPosition, rightForkPos);
+			
+			put_forks(philName, philPosition, rightForkPos);
+
+		}
+
+		// The left fork is free, but the right fork is not. So, give up: set down the left fork, adjust the semaphore, and try again in a bit.
+		else			
+		{
+			printf("Name: %s. Seat: %d. Status: HUNGRY. Forks: Has left fork, position %d. Can't have right fork, position %d. GIVING UP LEFT FORK.\n", philName, philPosition, philPosition, rightForkPos);
+			// unlock his right fork
+			pthread_mutex_unlock(&forkLocks[rightForkPos]);
+
+			// Remember: we adjusted the semaphore of the left fork already. Lock the fork, adjust the semaphore again, and unlock it.
+			pthread_mutex_lock(&forkLocks[philPosition]);
+			forks[philPosition] = true;
+			pthread_mutex_unlock(&forkLocks[philPosition]);
+		}
+	}
+
+	// The left fork is not available. We tried to lock the correct fork, so unlock it. We don't have to adjust the semaphore
+	// because we've done nothing to it.
+	else
+	{
+		printf("Name: %s. Seat: %d. Status: HUNGRY. Forks: Cannot have left fork, position %d, so won't check on his right fork, position %d.\n", philName, philPosition, philPosition, getRightForkPosition(philPosition));
+		pthread_mutex_unlock(&forkLocks[philPosition]);
+	}		 
+
 }
 
 // ***
@@ -93,77 +177,8 @@ void* lifeAsAPhilosopher(void* arg)
 	{
 		think(philName, philPosition);
 
-		printf("Philosopher: %s. Seat position: %d. Status: HUNGRY. Forks: Checking left fork, position %d.\n", philName, philPosition, philPosition);
-		
-		// Tentatively lock the philosopher's left fork in preparation for a feast.
-		// This'll fail if it's already locked.
-		pthread_mutex_lock(&forkLocks[philPosition]); 
-		
-		// Check the left fork using the shared bool resource that is locked from the statement above.
-		if (isForkAvailable(philPosition) == true) 
-		{
-			printf("Philosopher: %s. Seat position: %d. Status: HUNGRY. Forks: HAS left fork, position %d.\n", philName, philPosition, philPosition);
-			
-			// That fork is now unavailable. Alert the semaphore.
-			forks[philPosition] = false;
-
-			// Now that the semaphore is alerted, we have no need for the lock.
-			pthread_mutex_unlock(&forkLocks[philPosition]);
-
-			// Handle the right fork.
-			// Attempt to lock the global resources in order to modify them
-			// Check the semaphore to see if his right fork is free.
-			// If it is, unlock the global resources AFTER notifying the semaphore the right fork is no longer free.
-			// Then, lock both forks, change the global resources, then unlock the forks again.
-			int rightForkPos = getRightForkPosition(philPosition);
-
-			pthread_mutex_lock(&forkLocks[rightForkPos]);
-
-			printf("Philosopher: %s. Seat position: %d. Status: HUNGRY. Forks: HAS left fork, position %d, checking on right fork, position: %d.\n", philName, philPosition, philPosition, rightForkPos);
-			
-			// his right fork is free
-			if (isForkAvailable(rightForkPos) == true) 
-			{
-				forks[rightForkPos] = false;
-				
-				pthread_mutex_unlock(&forkLocks[rightForkPos]);
-
-				eat(philName, philPosition, rightForkPos);
-		
-				// Lock the forks
-				pthread_mutex_lock(&forkLocks[philPosition]);
-				pthread_mutex_lock(&forkLocks[rightForkPos]);
-	
-				// Adjust the semaphore: now the forks are available
-				forks[philPosition] = true;
-				forks[rightForkPos] = true;
-
-				// Unlock the forks now that the global resource is adjusted
-				pthread_mutex_unlock(&forkLocks[philPosition]);
-				pthread_mutex_unlock(&forkLocks[rightForkPos]);	
-			}
-
-			// The left fork is free, but the right fork is not. So, give up: set down the left fork, adjust the semaphore, and try again in a bit.
-			else			
-			{
-				printf("Philosopher: %s. Seat position: %d. Status: HUNGRY. Forks: Has left fork, position %d. Can't have right fork, position %d. GIVING UP LEFT FORK.\n", philName, philPosition, philPosition, rightForkPos);
-				// unlock his right fork
-				pthread_mutex_unlock(&forkLocks[rightForkPos]);
-
-				// Remember: we adjusted the semaphore of the left fork already. Lock the fork, adjust the semaphore again, and unlock it.
-				pthread_mutex_lock(&forkLocks[philPosition]);
-				forks[philPosition] = true;
-				pthread_mutex_unlock(&forkLocks[philPosition]);
-			}
-		}
-
-		// The left fork is not available. We tried to lock the correct fork, so unlock it. We don't have to adjust the semaphore
-		// because we've done nothing to it.
-		else
-		{
-			printf("Philosopher: %s. Seat position: %d. Status: HUNGRY. Forks: Cannot have left fork, position %d, so won't check on his right fork, position %d.\n", philName, philPosition, philPosition, getRightForkPosition(philPosition));
-			pthread_mutex_unlock(&forkLocks[philPosition]);
-		}		 
+		get_forks(phil, philName, philPosition);	
+ 
 
 		// Nifty function alerts the CPU to relinquish processing time from this thread. Tells the scheduler to place 
 		// this thread into the back of the run queue.		
