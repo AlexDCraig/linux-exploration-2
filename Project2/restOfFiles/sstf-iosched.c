51983361d56c479e4a2f46020353aaf9f87d058c
AlexDHoffer <hoffera@oregonstate.edu>
2017-05-04 19:40:00 -0700

    fixed everything, now final tarball for submission in /tarball

--- Project2/restOfFiles/sstf-iosched.c
+++ Project2/restOfFiles/sstf-iosched.c
@@ -18,44 +18,41 @@ static void sstf_merged_requests(struct request_queue *q, struct request *rq,
 				 struct request *next)
 {
 	list_del_init(&next->queuelist);
+	elv_dispatch_sort(q, next);
 }
 
 static int sstf_dispatch(struct request_queue *q, int force)
 {
+	// print whether data is being read or written
+	char direction;
 	struct sstf_data *nd = q->elevator->elevator_data;
+	struct request *rq = NULL;
 	printk("CLOOK algorithm has reached dispatch function\n");
 
-	if (!list_empty(&nd->queue)) {
-		struct request *rq;
+	rq = list_first_entry_or_null(&nd->queue, struct request, queuelist);
 
-		rq = list_entry(nd->queue.next, struct request, queuelist);
+	if (rq)
+	{
 		list_del_init(&rq->queuelist);
-		elv_dispatch_sort(q, rq);
-
-		//assign a position to the disk head
+		elv_dispatch_sort(q,rq);
 		diskhead = blk_rq_pos(rq);
+		if (rq_data_dir(rq) == READ)
+			direction = 'R';
 
-		//print whether data is being read or being written
-		char direction;
-		if(rq_data_dir(rq) == READ)
-		{
-            direction = 'R';
-		printk("Data is being read\n");
-		}
 		else
-		{
-            direction = 'W';
-		printk("Data is being written\n");
-		}
-		printk("[SSTF] Dispatching. Direction:  %c Sector: %lu\n", direction, blk_rq_pos(rq));
+			direction = 'W';
 
+		printk("CLOOK Dispatch. Direction: %c. Sector: %lu\n", direction, (unsigned long) blk_rq_pos(rq));	
 		return 1;
 	}
+
 	return 0;
+
 }
 
 static void sstf_add_request(struct request_queue *q, struct request *rq)
 {
+	char direction;
 	struct sstf_data *nd = q->elevator->elevator_data;
 	struct list_head *cur = NULL;
 
@@ -63,11 +60,11 @@ static void sstf_add_request(struct request_queue *q, struct request *rq)
 	list_for_each(cur, &nd->queue)
 	{
 		struct request *c = list_entry(cur, struct request, queuelist);
-		if(blk_rq_pos(rq) < diskhead) // check if request is bigger than disk head
+		if(blk_rq_pos(rq) > diskhead) // check if request is bigger than disk head
 		{
             //keep servicing bigger requests until it comes across a smaller request
             //insert when current is smaller than the head and bigger than the request
-			if(blk_rq_pos(c) < diskhead && blk_rq_pos(rq) < blk_rq_pos(c))
+			if(blk_rq_pos(c) < diskhead || blk_rq_pos(rq) < blk_rq_pos(c))
             {
                     break;
             }
@@ -76,31 +73,22 @@ static void sstf_add_request(struct request_queue *q, struct request *rq)
 		else
 		{
 		//find where current is smaller than the disk head or current smaller than request
-			if(blk_rq_pos(c) < diskhead ||
+			if(blk_rq_pos(c) < diskhead &&
 			   blk_rq_pos(rq) < blk_rq_pos(c))
 				break;
 		}
 	}
-
-	char direction;
 
 	//print out whether data is being read or written by checking direction
 	if(rq_data_dir(rq) == READ)
 		direction = 'R';
 	else
 		direction = 'W';
-	printk("[SSTF] add %c %lu\n", direction, blk_rq_pos(rq));
+	printk("[SSTF] add %c %lu\n", direction, (unsigned long)blk_rq_pos(rq));
 
 	//add to end of queue
 	list_add_tail(&rq->queuelist, cur);
 
-}
-
-static int sstf_queue_empty(struct request_queue *q)
-{
-	struct sstf_data *nd = q->elevator->elevator_data;
-
-	return list_empty(&nd->queue);
 }
 
 static struct request *
@@ -110,28 +98,36 @@ sstf_former_request(struct request_queue *q, struct request *rq)
 
 	if (rq->queuelist.prev == &nd->queue)
 		return NULL;
-	return list_entry(rq->queuelist.prev, struct request, queuelist);
+	return list_prev_entry(rq, queuelist);
 }
 
-static struct request *
-sstf_latter_request(struct request_queue *q, struct request *rq)
-{
-	struct sstf_data *nd = q->elevator->elevator_data;
-
-	if (rq->queuelist.next == &nd->queue)
-		return NULL;
-	return list_entry(rq->queuelist.next, struct request, queuelist);
-}
-
-static void *sstf_init_queue(struct request_queue *q)
+static int sstf_init_queue(struct request_queue *q, struct elevator_type *e)
 {
 	struct sstf_data *nd;
+	struct elevator_queue *eq;
+
+	eq = elevator_alloc(q,e);
+
+	if (!eq)
+		return -ENOMEM;
 
 	nd = kmalloc_node(sizeof(*nd), GFP_KERNEL, q->node);
+
 	if (!nd)
-		return NULL;
+	{
+		kobject_put(&eq->kobj);
+		return -ENOMEM;
+	}
+
+	eq->elevator_data = nd;
+
 	INIT_LIST_HEAD(&nd->queue);
-	return nd;
+
+	spin_lock_irq(q->queue_lock);
+	q->elevator = eq;
+	spin_unlock_irq(q->queue_lock);
+
+	return 0;
 }
 
 static void sstf_exit_queue(struct elevator_queue *e)
@@ -147,9 +143,7 @@ static struct elevator_type elevator_sstf = {
 		.elevator_merge_req_fn		= sstf_merged_requests,
 		.elevator_dispatch_fn		= sstf_dispatch,
 		.elevator_add_req_fn		= sstf_add_request,
-		.elevator_queue_empty_fn	= sstf_queue_empty,
 		.elevator_former_req_fn		= sstf_former_request,
-		.elevator_latter_req_fn		= sstf_latter_request,
 		.elevator_init_fn		= sstf_init_queue,
 		.elevator_exit_fn		= sstf_exit_queue,
 	},
@@ -159,8 +153,7 @@ static struct elevator_type elevator_sstf = {
 
 static int __init sstf_init(void)
 {
-	elv_register(&elevator_sstf);
-	return 0;
+	return elv_register(&elevator_sstf);
 }
 
 static void __exit sstf_exit(void)
