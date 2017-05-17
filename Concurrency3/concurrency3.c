6ce0edac8c8dc12d31fd09ab81961c19181ecffa
AlexDHoffer <hoffera@oregonstate.edu>
2017-05-16 18:06:46 -0700

    Finish concurrency3.

--- Concurrency3/concurrency3.c
+++ Concurrency3/concurrency3.c
@@ -7,9 +7,9 @@
 /* Synchronization variables */
 
 // Our three kinds of threads.
-pthread_t searcherThread[3]; // these examine the list, can execute concurrenctly with each other
-pthread_t deleterThread[3]; // these remove items from somewhere in the list, only 1 of these can access the list at once, and must lock the list when they do
-pthread_t inserterThread[3]; // these add new items to the end of the list, they have to lock the list when they're doing it, 1 insert can be running in parallel with any # of searches
+pthread_t searcherThread[2]; // these examine the list, can execute concurrenctly with each other
+pthread_t deleterThread[2]; // these remove items from somewhere in the list, only 1 of these can access the list at once, and must lock the list when they do
+pthread_t inserterThread[2]; // these add new items to the end of the list, they have to lock the list when they're doing it, 1 insert can be running in parallel with any # of searches
 
 // Our two kinds of Mutual Exclusion semaphores (mutexes). Don't need a third because the searcherThread requires no lock.
 pthread_mutex_t inserterMutex;
@@ -97,7 +97,7 @@ void insert(int num)
     }
     if(c==0)
         addToStart(num);
-    else if(c<count())
+    else if(c<sizeOfList())
         addAfterLocation(num,++c);
     else
         addToEnd(num);
@@ -135,6 +135,54 @@ int deleteFromList(int num)
     }
     return 0;
 }
+
+void initializeList();
+
+void deleteFromLocation(int loc)
+{
+
+	pthread_t ID = pthread_self();
+
+ 	if (head == NULL)
+	{
+		printf("DELETER, ID %lu: Cannot delete index, list is empty.\n", (unsigned long) ID);
+		return;
+	}
+
+	struct node* temp = head;
+      
+	if (loc == 0)
+	{
+		printf("DELETER, ID %lu: Deleting head, which is value %d\n", (unsigned long) ID, head->data);
+		head = temp->next;
+		free(temp);
+		initializeList();
+		return;
+	}
+
+	int i = 0;
+
+	for (i; temp != NULL && i<loc-1; i++)
+		temp = temp->next;
+
+	if (temp == NULL || temp->next == NULL)
+	{
+		printf("DELETER, ID %lu: Invalid index\n", (unsigned long) ID);
+		return;
+	}
+
+	struct node* next = temp->next->next;
+	int data = temp->next->data;
+
+	free(temp->next);
+
+	temp->next = next;	
+
+	printf("DELETER, ID %lu: Deleted index %d which held value %d\n", (unsigned long) ID, loc, data);	 
+
+	return;
+}
+ 
  
 void printList(struct node *r)
 {
@@ -165,6 +213,34 @@ int sizeOfList()
     return c;
 }
 
+void findValue(int val)
+{
+	pthread_t ID = pthread_self();
+	int found = 0;
+
+
+    struct node* r=head;
+    if(r==NULL)
+    {
+    printf("SEARCHER, ID %lu: The list is empty. Cannot find %d.\n", (unsigned long) ID, val);
+    return;
+    }
+    while(r!=NULL)
+    {
+	if (r->data == val)
+	{
+		printf("SEARCHER, ID %lu: Found value %d \n", (unsigned long) ID,r->data);
+		found = 1;
+		break;
+	}
+
+    	r=r->next;
+    }
+ 	if (found == 0)
+  	printf("SEARCHER, ID %lu: Value %d is not in the list\n", (unsigned long) ID, val);
+
+}
+
 /* Methods for the threads to load when they are created. */
 
 // Algo: 1) Come up with random #
@@ -174,7 +250,26 @@ int sizeOfList()
 // 5) sleep for a bit
 void* searcher()
 {
-		
+	while(1)
+	{
+	int value;
+	sem_getvalue(&deleterSearcherSem, &value);
+
+	pthread_t ID = pthread_self();
+	
+	if (value == 0)
+		printf("SEARCHER, ID %lu: Is blocked. Will wait for semaphore to alert it.\n", (unsigned long) ID);
+	
+	sem_wait(&deleterSearcherSem);
+	
+	int random = rand() % 6;
+
+	findValue(random);		
+
+	sem_post(&deleterSearcherSem);	
+
+	sleep(3);
+	}
 }
 
 // Algo: 1) Wait until both semaphores available to lock to make sure inserter or search methods aren't using the resources at the same time: sem_wait(&deleterInserterSem), sem_wait(&deleterSearcherSem)
@@ -186,7 +281,39 @@ void* searcher()
 // 6) sleep for a bit
 void* deleter()
 {
+	while(1)
+	{
+	pthread_t ID = pthread_self();
+	
 
+	int value;
+	sem_getvalue(&deleterSearcherSem, &value);
+
+	if (value == 0)
+		printf("DELETER, ID %lu: Is blocked. Will wait for semaphore to alert it.\n", (unsigned long) ID);
+	
+	sem_wait(&deleterSearcherSem);
+
+	sem_getvalue(&deleterInserterSem, &value);
+
+	if (value == 0)
+		printf("DELETER, ID %lu: Is blocked. Will wait for semaphore to alert it.\n", (unsigned long) ID);
+	
+	sem_getvalue(&deleterInserterSem, &value);
+	
+	sem_wait(&deleterInserterSem);
+
+	pthread_mutex_lock(&deleterMutex);
+
+	int randomIndex = rand() % sizeOfList(head);
+	deleteFromLocation(randomIndex);
+
+	pthread_mutex_unlock(&deleterMutex);
+	sem_post(&deleterSearcherSem);
+	sem_post(&deleterInserterSem);
+
+	sleep(6);
+	}
 }
 
 // Algo: 1) Wait for the deleter semaphore to be available:
@@ -200,29 +327,59 @@ void* deleter()
 // pthread_mutex_unlock(&inserterMutex)
 void* inserter()
 {
+	while(1)
+	{
+	pthread_t ID = pthread_self();
+	
+	int value;
+	sem_getvalue(&deleterInserterSem, &value);
 
+	if (value == 0)
+		printf("INSERTER, ID %lu: Is blocked. Will wait for semaphore to alert it.\n", (unsigned long) ID);
+
+	sem_wait(&deleterInserterSem);
+	pthread_mutex_lock(&inserterMutex);
+
+	int randomNumber = rand() % 6;
+	addToEnd(randomNumber);
+	printf("INSERTER, ID %lu: Added %d to end of list\n", (unsigned long) ID, randomNumber);	
+
+	sem_post(&deleterInserterSem);
+	pthread_mutex_unlock(&inserterMutex);
+	sleep(3);
+	}
+}
+
+void initializeList()
+{
+	head = malloc(sizeof(struct node));
+	head->next = NULL;
+	int newData = rand() % 5;
+	head->data = newData;
 }
 
 int main()
 {
 	srand(time(NULL));
 
-	pthread_mutex_init(&inserterMutex);
-	pthread_mutex_init(&deleterMutex);
+	initializeList();
 
-	sem_init(&deleterInserterSem);
-	sem_init(&deleterSearcherSem);
+	pthread_mutex_init(&inserterMutex, NULL);
+	pthread_mutex_init(&deleterMutex, NULL);
+
+	sem_init(&deleterInserterSem, 0, 1);
+	sem_init(&deleterSearcherSem, 0, 1);
 
 	int k = 0;
-	for (k; k < 3; k++)
+	for (k; k < 2; k++)
 	{
-		pthread_create(&searcherThread[k], NULL, searcher);
-		pthread_create(&deleterThread[k], NULL, deleter);
-		pthread_create(&inserterThread[k], NULL, inserter);
+		pthread_create(&searcherThread[k], NULL, searcher, NULL);
+		pthread_create(&deleterThread[k], NULL, deleter, NULL);
+		pthread_create(&inserterThread[k], NULL, inserter, NULL);
 	}
 
 	k = 0;
-	for (k; k < 3; k++)
+	for (k; k < 2; k++)
 	{
 		pthread_join(searcherThread[k], NULL);
 		pthread_join(deleterThread[k], NULL);
